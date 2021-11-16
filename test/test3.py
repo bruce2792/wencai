1b4e8bdcb02e594e575601c0893c1b356cc2256b
bruce2792 <279225276@qq.com>
2021-11-16 16:58:45 +0800

    知识改变生活

--- test/test3.py
+++ test/test3.py
@@ -8,7 +8,7 @@ import json
 # print(result)
 
 # 1794.08 1794.02 1794.00 A股现价以及昨日收盘价 A股上市日期降序 A股上市日期降序
-result = wc.search(query="A股市值降序", page=1, pageSize=5000)
+result = wc.search(query="A股换手率排名", page=1, pageSize=1)
 print(result)
 
 # result = wc.search(query="A股上市日期降序", page=1, pageSize=1000)
